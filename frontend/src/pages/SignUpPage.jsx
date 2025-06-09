a274cd895dc1a3049ba8a4b02676332c37d9c459
SHREE <SHREE@DESKTOP-VAQMQK5>
2025-06-09 19:00:59 +0530

    Design SignUp and login page

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -2,7 +2,6 @@ import { Fragment, useState } from "react";
 import { Link } from "react-router-dom";
 import { UserPlus, Mail, Lock, User, ArrowRight, Loader } from "lucide-react";
 import { motion } from "framer-motion";
-import Input from "../utils/Input";
 // import { useUserStore } from "../stores/useUserStore";
 
 export default function SignUpPage() {
@@ -23,6 +22,8 @@ export default function SignUpPage() {
   const handleChange = (fieldName, value) => {
     setFormData({ ...formData, [fieldName]: value });
   };
+
+  const loading = false;
 
   return (
     <Fragment>
@@ -140,7 +141,71 @@ export default function SignUpPage() {
                   />
                 </div>
               </div>
+              <div>
+                <label
+                  htmlFor="confirmPassword"
+                  className="block text-sm font-medium text-gray-300"
+                >
+                  Confirm Password
+                </label>
+                <div className="mt-1 relative rounded-md shadow-sm">
+                  <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                    <Lock
+                      className="h-5 w-5 text-gray-400"
+                      aria-hidden="true"
+                    />
+                  </div>
+                  <input
+                    id="confirmPassword"
+                    type="password"
+                    required
+                    value={formData.confirmPassword}
+                    onChange={(e) =>
+                      setFormData({
+                        ...formData,
+                        confirmPassword: e.target.value,
+                      })
+                    }
+                    className=" block w-full px-3 py-2 pl-10 bg-gray-700 border
+									 border-gray-600 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 sm:text-sm"
+                    placeholder="••••••••"
+                  />
+                </div>
+              </div>
+
+              <button
+                type="submit"
+                className="w-full flex justify-center py-2 px-4 border border-transparent 
+							rounded-md shadow-sm text-sm font-medium text-white bg-emerald-600
+							 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2
+							  focus:ring-emerald-500 transition duration-150 ease-in-out disabled:opacity-50"
+                disabled={loading}
+              >
+                {loading ? (
+                  <>
+                    <Loader
+                      className="mr-2 h-5 w-5 animate-spin"
+                      aria-hidden="true"
+                    />
+                    Loading...
+                  </>
+                ) : (
+                  <>
+                    <UserPlus className="mr-2 h-5 w-5" aria-hidden="true" />
+                    Sign up
+                  </>
+                )}
+              </button>
             </form>
+            <p className="mt-8 text-center text-sm text-gray-400">
+              Already have an account?{" "}
+              <Link
+                to="/login"
+                className="font-medium text-emerald-400 hover:text-emerald-300"
+              >
+                Login here <ArrowRight className="inline h-4 w-4" />
+              </Link>
+            </p>
           </div>
         </motion.div>
       </div>
